4e34e9e3f50d101aaa3fa4c5afb4681805717132
Jeremy Lacomis <jlacomis@cmu.edu>
2019-09-10 18:15:57 -0400

    Move text drawing to lib

---
+++ lib/drawing.ml
@@ -0,0 +1,38 @@
+(* Coordinate in the bottom-left *)
+let draw_string_with_box string x y =
+  let open Graphics in
+  let orig_x, orig_y = current_point () in
+  moveto (x + 5) y;
+  let width, height = text_size string in
+  set_color black;
+  fill_rect x y (width + 10) height;
+  set_color white;
+  draw_string string;
+  moveto orig_x orig_y;
+  synchronize ()
+
+(* Coordinate in the bottom-right *)
+let draw_string_with_box_br string x y =
+  let width, _ = Graphics.text_size string in
+  let x = x - width - 10 in
+  draw_string_with_box string x y
+
+(* Coordinate in the top-right *)
+let draw_string_with_box_tr string x y =
+  let width, height = Graphics.text_size string in
+  let x = x - width - 10 in
+  let y = y - height in
+  draw_string_with_box string x y
+
+(* Coordinate in the top-left *)
+let draw_string_with_box_tl string x y =
+  let _, height = Graphics.text_size string in
+  let y = y - height in
+  draw_string_with_box string x y
+
+(* Coordinate in the center *)
+let draw_string_with_box_c string x y =
+  let width, height = Graphics.text_size string in
+  let x = x - (width / 2) in
+  let y = y - (height / 2) in
+  draw_string_with_box string x y
--- src/main.ml
+++ src/main.ml
@@ -1,3 +1,5 @@
+open Drawing
+
 type point =
   { x : float
   ; y : float
@@ -107,45 +109,6 @@ let get_rectangle (fst_event : Graphics.status) range =
   let min = { x = rescale_x new_min_x; y = rescale_y new_min_y } in
   let max = { x = rescale_x new_max_x; y = rescale_y new_max_y } in
   { min; max }
-
-(* Coordinate in the bottom-left *)
-let draw_string_with_box string x y =
-  let open Graphics in
-  let orig_x, orig_y = current_point () in
-  moveto (x + 5) y;
-  let width, height = text_size string in
-  set_color black;
-  fill_rect x y (width + 10) height;
-  set_color white;
-  draw_string string;
-  moveto orig_x orig_y;
-  synchronize ()
-
-(* Coordinate in the bottom-right *)
-let draw_string_with_box_br string x y =
-  let width, _ = Graphics.text_size string in
-  let x = x - width - 10 in
-  draw_string_with_box string x y
-
-(* Coordinate in the top-right *)
-let draw_string_with_box_tr string x y =
-  let width, height = Graphics.text_size string in
-  let x = x - width - 10 in
-  let y = y - height in
-  draw_string_with_box string x y
-
-(* Coordinate in the top-left *)
-let draw_string_with_box_tl string x y =
-  let _, height = Graphics.text_size string in
-  let y = y - height in
-  draw_string_with_box string x y
-
-(* Coordinate in the center *)
-let draw_string_with_box_c string x y =
-  let width, height = Graphics.text_size string in
-  let x = x - (width / 2) in
-  let y = y - (height / 2) in
-  draw_string_with_box string x y
 
 let draw_scale range =
   let open Graphics in
