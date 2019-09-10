de6ad425132052004d184312ea2c1e36a1c592a9
Jeremy Lacomis <jlacomis@cmu.edu>
2019-09-10 19:17:56 -0400

    Move open Graphics to top of file

--- lib/drawing.ml
+++ lib/drawing.ml
@@ -1,6 +1,7 @@
+open Graphics
+
 (* Coordinate in the bottom-left *)
 let draw_string_with_box string x y =
-  let open Graphics in
   let orig_x, orig_y = current_point () in
   moveto (x + 5) y;
   let width, height = text_size string in
@@ -13,26 +14,26 @@ let draw_string_with_box string x y =
 
 (* Coordinate in the bottom-right *)
 let draw_string_with_box_br string x y =
-  let width, _ = Graphics.text_size string in
+  let width, _ = text_size string in
   let x = x - width - 10 in
   draw_string_with_box string x y
 
 (* Coordinate in the top-right *)
 let draw_string_with_box_tr string x y =
-  let width, height = Graphics.text_size string in
+  let width, height = text_size string in
   let x = x - width - 10 in
   let y = y - height in
   draw_string_with_box string x y
 
 (* Coordinate in the top-left *)
 let draw_string_with_box_tl string x y =
-  let _, height = Graphics.text_size string in
+  let _, height = text_size string in
   let y = y - height in
   draw_string_with_box string x y
 
 (* Coordinate in the center *)
 let draw_string_with_box_c string x y =
-  let width, height = Graphics.text_size string in
+  let width, height = text_size string in
   let x = x - (width / 2) in
   let y = y - (height / 2) in
   draw_string_with_box string x y
